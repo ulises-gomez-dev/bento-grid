b9ce8eb144cae96013586979beb233fa35df6518
Ulises Gomez <usg@arizona.edu>
2025-05-08 21:55:29 -0700

    added @media ruleset; bento grid jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import FiveStars from "./assets/images/illustration-five-stars.webp";
 
 function App() {
   return (
-    <>
-      <h1>Bento Grid</h1>
-    </>
+    <div className="bento-grid">
+      <article id="five-stars">
+        <h1>Social Media 10x Faster with AI</h1>
+        <img src={FiveStars} alt="" />
+        <p>Over 4,000 5-star reviews</p>
+      </article>
+      <article id="multiple-platforms"></article>
+      <article id="consistent-schedule"></article>
+      <article id="schedule-posts-card"></article>
+      <article id="grow-followers"></article>
+      <article id="audience-growth"></article>
+      <article id="create-post"></article>
+      <article id="ai-content"></article>
+    </div>
   );
 }
 
